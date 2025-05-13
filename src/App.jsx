40cec072825ec416ba823ba1c0b451803da1c6a7
mhmd329 <mhmdkhaled092@gmail.com>
2025-05-13 18:37:40 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/checkout" element={<Checkout />} />
-            <Route path="/ze/orders" element={<AdminOrders />} />
+            <Route path="/zeko/orders" element={<AdminOrders />} />
             <Route path="/product/:id" element={<ProductDetails/>} />
           </Routes>
         </main>
